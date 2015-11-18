74450dc3f2caeff97238593176022ca271450d3a
AmirRajabifar <amir_rajabifar@yahoo.com.au>
2015-11-18 19:21:53 +0800

    First commit
    Adding the codes. Let the fun BEGIN!!!

---
+++ Read-pot/Read_pot_and_send/Read_pot_and_send.ino
@@ -0,0 +1,24 @@
+
+
+int potPin = 0;
+
+void setup()
+{
+  //Create Serial Object (9600 Baud)
+  Serial.begin(250000);
+
+}
+
+void loop()
+{
+  int pot = map(analogRead(potPin), 0, 1023, 178,214);
+  Serial.print(pot, DEC);
+  Serial.print(",");
+  int SinPot = map(analogRead(sin(potPin)),0, 1023, 1650, 300);
+  Serial.println(SinPot, DEC);
+  
+  delay(50);
+  
+}
+
+
