6f4ef34346f8bcde4131f5bc6db4af3bd88a929b
AmirRajabifar <amir_rajabifar@yahoo.com.au>
2015-11-26 14:26:07 +0800

    Phase 1
    adding the commands to listen to CANbus and do some processing and
    diagnostics

--- Car_Display_processor/Car_Display_processor.ino
+++ Car_Display_processor/Car_Display_processor.ino
@@ -3,66 +3,123 @@
  */
 
 // including required liberaries 
-#include <KellyCAN.h>
+//#include <KellyCAN.h>
 #include <FlexCAN.h>
 #include <CANcallbacks.h>
 #include <ChallengerEV.h>
 
-
 //wheels are indexed 1-4
-const int wheelnumber = 3;
+//const int wheelnumber = 3;
 
 
 
 
-const int wheelnum = wheelnumber - 1;
+//const int wheelnum = wheelnumber - 1;
 
-const int brakeDacPin = 20;   
-const int throttleDacPin = 21;
-const int brSwOut = 14;         
-const int thSwOut = 15;
-const int reSwOut = 16;
-
-const int redLightInPin = 22;
-const int greenLightInPin = 23;
-
-/* Think we don't need this
 //pull the constants out of ChallengerEV.h
-const uint32_t ManagerID  = wheel[wheelnum].managerID;  //messages to the wheel managers
-const uint32_t KellyreqID = wheel[wheelnum].motorReqID; //messages to the kelly
-const uint32_t KellyresID = wheel[wheelnum].motorResID; //messages from the kelly
-const uint32_t dashID = wheel[wheelnum].dashID;  //telemetry messages from the wheel manager
-*/
+
+const uint32_t dashID1 = wheel[1].dashID;  //telemetry messages from the wheel manager
+const uint32_t dashID2= wheel[2].dashID;  //telemetry messages from the wheel manager
+const uint32_t dashID3 = wheel[3].dashID;  //telemetry messages from the wheel manager
+const uint32_t dashID4 = wheel[4].dashID;  //telemetry messages from the wheel manager
 
 // I D K
 FlexCAN CANbus(1000000);
 CANcallbacks canbus(&CANbus);
-KellyCAN motor(&canbus, KellyreqID, KellyresID);
 
-/* neither this
-//below are the messages defined in the datasheet.  The flash reads look a whole lot like memory offsets.
-CAN_message_t known_messages[] = { 
-  {KellyreqID,0,3,0,CCP_FLASH_READ,INFO_MODULE_NAME,8,0,0,0,0,0},
-  {KellyreqID,0,3,0,CCP_FLASH_READ,INFO_SOFTWARE_VER,2,0,0,0,0,0},
-  {KellyreqID,0,3,0,CCP_FLASH_READ,CAL_TPS_DEAD_ZONE_LOW,1,0,0,0,0,0},
-  {KellyreqID,0,3,0,CCP_FLASH_READ,CAL_TPS_DEAD_ZONE_HIGH,1,0,0,0,0,0},
-  {KellyreqID,0,3,0,CCP_FLASH_READ,CAL_BRAKE_DEAD_ZONE_LOW,1,0,0,0,0,0},
-  {KellyreqID,0,3,0,CCP_FLASH_READ,CAL_BRAKE_DEAD_ZONE_HIGH,1,0,0,0,0,0},
-  {KellyreqID,0,1,0,CCP_A2D_BATCH_READ1,0,0,0,0,0,0,0},
-  {KellyreqID,0,1,0,CCP_A2D_BATCH_READ2,0,0,0,0,0,0,0},
-  {KellyreqID,0,1,0,CCP_MONITOR1,0,0,0,0,0,0,0},
-  {KellyreqID,0,1,0,CCP_MONITOR2,0,0,0,0,0,0,0},
-  {KellyreqID,0,2,0,COM_SW_ACC,COM_READING,0,0,0,0,0,0},
-  {KellyreqID,0,2,0,COM_SW_BRK,COM_READING,0,0,0,0,0,0},
-  {KellyreqID,0,2,0,COM_SW_REV,COM_READING,0,0,0,0,0,0}
-};
-*/
+
+bool redLightBit = false;
+bool greenLightBit = false;
+float RPM;
+float voltage;
+float current;
+float throttle;
+float brake;
+
+void wheel1ProcessMessage (CAN_message_t &message){
+
+  
+}
+
+void wheel2ProcessMessage (CAN_message_t &message){
+
+  
+}
+void wheel3ProcessMessage (CAN_message_t &message){
+
+  
+}
+void wheel4ProcessMessage (CAN_message_t &message){
+
+  
+}
+void pedalsProcessMessage (CAN_message_t &message){
+
+  
+}
+
+
+// a function to get the data returned by each wheel and do some averaging
+
+
 void setup() {
-  // put your setup code here, to run once:
+  // 
 
+  Serial.begin(9600);
+  Serial.println("Dash Display");
+
+  CANbus.begin();
+
+  CAN_filter_t dashFilter1;
+  dashFilter1.rtr = 0;
+  dashFilter1.ext = 0;
+  dashFilter1.id = dashID1;
+
+  CAN_filter_t dashFilter2;
+  dashFilter2.rtr = 0;
+  dashFilter2.ext = 0;
+  dashFilter2.id = dashID2;
+
+  CAN_filter_t dashFilter3;
+  dashFilter3.rtr = 0;
+  dashFilter3.ext = 0;
+  dashFilter3.id = dashID3;
+
+  CAN_filter_t dashFilter4;
+  dashFilter4.rtr = 0;
+  dashFilter4.ext = 0;
+  dashFilter4.id = dashID4;
+  
+
+  CANbus.setFilter(dashFilter1,0);
+  CANbus.setFilter(dashFilter2,0);
+  CANbus.setFilter(dashFilter3,0);
+  CANbus.setFilter(dashFilter4,0);
+
+//fill the remaining filters to prevent ack.
+  for (int i = 2; i < 8; ++i)
+  {
+    CANbus.setFilter(dashFilter1,i);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // check for new message
+  CAN_message_t message;
 
+  while(canbus.receive(message)) {
+    for( int i=0; i<7; i++){
+      Serial.print(message.buf[i]);
+    }
+    Serial.println();
+  }
+  
+
+       
 }
+
+    
+  
+  
+
+
