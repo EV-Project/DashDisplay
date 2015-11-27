9c68049c32122d21f5f0a470c39420694cafa3c7
AmirRajabifar <amir_rajabifar@yahoo.com.au>
2015-11-27 18:22:15 +0800

    unpacking
    unpacking the CAN message

--- Car_Display_processor/Car_Display_processor.ino
+++ Car_Display_processor/Car_Display_processor.ino
@@ -18,41 +18,94 @@
 
 //pull the constants out of ChallengerEV.h
 
-const uint32_t dashID1 = wheel[1].dashID;  //telemetry messages from the wheel manager
-const uint32_t dashID2= wheel[2].dashID;  //telemetry messages from the wheel manager
-const uint32_t dashID3 = wheel[3].dashID;  //telemetry messages from the wheel manager
-const uint32_t dashID4 = wheel[4].dashID;  //telemetry messages from the wheel manager
-const uint32_t pedalID = wheel[1].pedalID;
+const uint32_t dashID1 = wheel[1].dashID;   //telemetry messages from the wheel manager
+const uint32_t dashID2= wheel[2].dashID;    //telemetry messages from the wheel manager
+const uint32_t dashID3 = wheel[3].dashID;   //telemetry messages from the wheel manager
+const uint32_t dashID4 = wheel[4].dashID;   //telemetry messages from the wheel manager
+const uint32_t pedalID = pedalID;           //telemetry messages from the wheel manager
 
 // I D K
   FlexCAN CANbus(1000000);
   CANcallbacks canbus(&CANbus);
 
 
-bool redLightBit = false;
-bool greenLightBit = false;
-uint16_t RPMbit;
-uint16_t RPM;
-uint8_t voltage;
-uint8_t current;
-uint8_t throttle;
-uint8_t brake;
+  bool redLightBit = false;
+  bool greenLightBit = false;
+  // variables for wheel 1
+  uint16_t RPM1;
+  uint8_t voltage1;
+  uint8_t current1;
+  uint8_t throttle1;
+  uint8_t brake1;
+  uint8_t telemBits1;
+  // variables for wheel 2
+  uint16_t RPM2;
+  uint8_t voltage2;
+  uint8_t current2;
+  uint8_t throttle2;
+  uint8_t brake2;
+  uint8_t telemBits2;
+  // variables for wheel 3
+  uint16_t RPM3;
+  uint8_t voltage3;
+  uint8_t current3;
+  uint8_t throttle3;
+  uint8_t brake3;
+  uint8_t telemBits3;
+  //variables for wheel 4
+  uint16_t RPM4;
+  uint8_t voltage4;
+  uint8_t current4;
+  uint8_t throttle4;
+  uint8_t brake4;
+  uint8_t telemBits4;
 
-bool wheel1ProcessMessage (CAN_message_t &message){
-
+  //varaibles to store the average value
+  uint32_t aRPM;
+  uint16_t avoltage;
+  uint16_t acurrent;
+  uint16_t athrottle;
+  uint16_t abrake;
+  uint16_t atelemBits;
   
+bool wheel1ProcessMessage (CAN_message_t &message){
+     RPM1 = ((uint16_t)message.buf[0])<<8;
+     RPM1 |= ((uint16_t)message.buf[1]);
+     voltage1 = message.buf[2];
+     current1 = message.buf[3];
+     throttle1 = message.buf[4];
+     brake1 = message.buf[5];
+     telemBits1 =message.buf[6];
 }
 
 bool wheel2ProcessMessage (CAN_message_t &message){
-
+     RPM2 = ((uint16_t)message.buf[0])<<8;
+     RPM2 |= ((uint16_t)message.buf[1]);
+     voltage2 = message.buf[2];
+     current2 = message.buf[3];
+     throttle2 = message.buf[4];
+     brake2 = message.buf[5];
+     telemBits2 = message.buf[6];
   
 }
 bool wheel3ProcessMessage (CAN_message_t &message){
-
+     RPM3 = ((uint16_t)message.buf[0])<<8;
+     RPM3 |= ((uint16_t)message.buf[1]);
+     voltage3 = message.buf[2];
+     current3 = message.buf[3];
+     throttle3 = message.buf[4];
+     brake3 = message.buf[5];
+     telemBits3 = message.buf[6];
   
 }
 bool wheel4ProcessMessage (CAN_message_t &message){
-
+     RPM4 = ((uint16_t)message.buf[0])<<8;
+     RPM4 |= ((uint16_t)message.buf[1]);
+     voltage4 = message.buf[2];
+     current4 = message.buf[3];
+     throttle4 = message.buf[4];
+     brake4 = message.buf[5];
+     telemBits4 = message.buf[6];
   
 }
 bool pedalProcessMessage (CAN_message_t &message){
@@ -122,19 +175,16 @@ void loop() {
   CAN_message_t message;
 
   while(canbus.receive(message)) {
-    RPMbit = message.buf[0];
-    RPM = message.buf[1];
-    voltage = message.buf[2];
-    current = message.buf[3];
-    throttle = message.buf[4];
-    brake = message.buf[5]; 
+
   }
-/*    for( int i=0; i<7; i++){
-      Serial.print(message.buf[i]);
-    }
-    Serial.println();
-  }
- */ 
+
+  aRPM =      (RPM1+RPM2+RPM3+RPM4)/4;
+  avoltage =  (voltage1+voltage2+voltage3+voltage4)/4;
+  acurrent =  (current1+current2+current3+current4)/4;
+  athrottle = (throttle1+throttle2+throttle3+throttle4)/4;
+  abrake =    (brake1+brake2+brake3+brake4)/4;
+  atelemBits =(telemBits1+telemBits2+telemBits3+telemBits4)/4;
+   
 
        
 }
